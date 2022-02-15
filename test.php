39ed709ac36b25f63c2f44c711f6ec22647aa5da
Dante-Cand <cand.dante@gmail.com>
2022-02-15 16:33:21 +0100

    first

---
+++ test.php
@@ -0,0 +1,22 @@
+declare(strict_types=1);
+<?php
+
+
+
+
+
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+echo "Exercise 1 starts here:";
+
+function new_exercise($x)
+{ 
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+}
+?>
+    
+</body>
+</html>
